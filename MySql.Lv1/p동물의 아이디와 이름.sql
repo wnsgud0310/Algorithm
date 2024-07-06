2989c31c17aeada431d892627af5a26d7877c9f3
wnsgud0310 <101302585+wnsgud0310@users.noreply.github.com>
2024-07-06 18:51:13 +0900

    Create p동물의 아이디와 이름.sql

---
+++ MySql.Lv1/p동물의 아이디와 이름.sql
@@ -0,0 +1,29 @@
+
+SELECT ANIMAL_ID,NAME
+from ANIMAL_INS
+order by ANIMAL_ID asc;
+
+
+
+/*
+ 1단계
+SELECT ANIMAL_ID,NAME
+from ANIMAL_INS
+
+
+ 2단계
+SELECT ANIMAL_ID,NAME
+from ANIMAL_INS
+order by ANIMAL_ID asc;
+
+
+
+
+
+
+
+
+
+
+
+ */
