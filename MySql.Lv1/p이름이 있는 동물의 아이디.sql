cc64bc02e5a7dea4265c45a7331d2dbd8ed587e3
wnsgud0310 <101302585+wnsgud0310@users.noreply.github.com>
2024-07-06 19:10:21 +0900

    Create p이름이 있는 동물의 아이디.sql

---
+++ MySql.Lv1/p이름이 있는 동물의 아이디.sql
@@ -0,0 +1,19 @@
+
+select ANIMAL_ID
+from ANIMAL_INS
+where name is not null;
+
+
+/*
+1단계;
+ select ANIMAL_ID
+ from ANIMAL_INS;
+
+2단계
+ select ANIMAL_ID
+from ANIMAL_INS
+where name is not null;
+이름이 있는 동물 조회
+
+
+ */
