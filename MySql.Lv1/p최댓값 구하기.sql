5210dccdaa091f283a1a96b7bdc41274f9d79723
wnsgud0310 <101302585+wnsgud0310@users.noreply.github.com>
2024-07-09 20:02:39 +0900

    Create p최댓값 구하기.sql

---
+++ MySql.Lv1/p최댓값 구하기.sql
@@ -0,0 +1,27 @@
+
+SELECT MAX(DATETIME) "시간"
+FROM ANIMAL_INS
+
+
+/*
+ 1단계
+    SELECT DATETIME as "시간"
+    from ANIMAL_INS
+    order by  DATETIME desc
+
+ 2단계
+SELECT DATETIME as "시간"
+from ANIMAL_INS
+order by  DATETIME desc
+Limit 1;
+
+
+ 3단계
+    SELECT MAX(DATETIME) "시간"
+    FROM ANIMAL_INS
+
+
+
+
+
+ */
