03a0ee2e779ba9e191d6516cb31bd1919cf1de15
wnsgud0310 <101302585+wnsgud0310@users.noreply.github.com>
2024-07-09 19:55:06 +0900

    Create p가장 비싼 상품 구하기.sql

---
+++ MySql.Lv1/p가장 비싼 상품 구하기.sql
@@ -0,0 +1,41 @@
+
+SELECT PRICE  MAX_PRICE
+from PRODUCT
+order by PRICE desc
+Limit 1;
+
+
+
+/*
+ 1단계
+    SELECT PRICE
+    from PRODUCT;
+
+
+
+ 2단계
+
+SELECT PRICE  MAX_PRICE
+from PRODUCT;
+
+
+
+
+
+ 3단계
+
+SELECT PRICE  MAX_PRICE
+from PRODUCT
+order by PRICE desc;
+
+ 4단계
+SELECT PRICE  MAX_PRICE
+from PRODUCT
+order by PRICE desc
+Limit 1;
+
+
+
+
+
+ */
