fc616944ceb28df03e9a0344b997a60103e3711d
wnsgud0310 <101302585+wnsgud0310@users.noreply.github.com>
2024-07-09 20:08:03 +0900

    Create p동물 수 구하기.sql
    count() 사용

---
+++ MySql.Lv1/p동물 수 구하기.sql
@@ -0,0 +1,28 @@
+SELECT count(ANIMAL_ID) as count
+from ANIMAL_INS;
+
+
+/*
+ 1단계
+    SELECT count(ANIMAL_ID) as count
+    from ANIMAL_INS;
+    count() 사용
+  () 안에  뭐가들어가도 몇명인지 알수있음
+
+
+ 2단계
+
+
+
+
+
+
+ 3단계
+
+
+
+
+
+
+
+ */
