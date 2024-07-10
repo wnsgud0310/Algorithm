30feaebebeb47bc408b6a5f8e278c4286e430736
wnsgud0310 <101302585+wnsgud0310@users.noreply.github.com>
2024-07-10 13:28:53 +0900

    Create p인기있는 아이스크림.sql
    desc 내림차순
    asc 오름차순

---
+++ MySql.Lv1/p인기있는 아이스크림.sql
@@ -0,0 +1,33 @@
+SELECT FLAVOR
+from FIRST_HALF
+order by TOTAL_ORDER desc, SHIPMENT_ID asc;
+
+
+
+/*
+ 1단계
+SELECT FLAVOR
+from FIRST_HALF
+
+
+
+ 2단계
+SELECT FLAVOR
+from FIRST_HALF
+order by TOTAL_ORDER desc, SHIPMENT_ID asc;
+
+ desc: 내림차순
+ asc : 오름차순 , 기본값이라 asc 안붙여도됨
+
+
+
+
+ 3단계
+
+
+
+
+
+
+
+ */
