27c58465101c664381626c70f9f7df212a3137a8
wnsgud0310 <101302585+wnsgud0310@users.noreply.github.com>
2024-07-10 13:11:47 +0900

    Create p이름이 없는 동물의 아이디.sql
    is null 
    이름이 없는 동물 아이디 조회

---
+++ MySql.Lv1/p이름이 없는 동물의 아이디.sql
@@ -0,0 +1,42 @@
+SELECT ANIMAL_ID
+from ANIMAL_INS
+where NAME is  null
+order by ANIMAL_ID asc;
+
+
+
+
+/*
+ 1단계
+
+SELECT ANIMAL_ID
+from ANIMAL_INS ;
+
+
+
+
+
+ 2단계
+SELECT ANIMAL_ID
+from ANIMAL_INS
+where NAME is  null;
+
+
+
+
+
+ 3단계
+SELECT ANIMAL_ID
+from ANIMAL_INS
+where NAME is  null
+order by ANIMAL_ID asc;
+
+
+
+
+
+
+
+
+
+ */
