bad23c30eed6d5fd033a1fff9d61c6d8fca7fb55
wnsgud0310 <101302585+wnsgud0310@users.noreply.github.com>
2024-07-11 23:22:11 +0900

    Create p모든 레코드 조회하기.sql
    모든 정보 조회 
    select *

---
+++ MySql.Lv1/p모든 레코드 조회하기.sql
@@ -0,0 +1,34 @@
+
+SELECT *
+from ANIMAL_INS
+order by ANIMAL_ID asc;
+
+
+
+/*
+ 1단계
+SELECT *
+from ANIMAL_INS
+
+
+
+ 2단계
+SELECT *
+from ANIMAL_INS
+order by ANIMAL_ID asc;
+
+ 모든 동물의 정보이기 떄문에 select * 하면 된다.
+ 동물 아이디 순으로 조히하기 때문에 asc;
+
+
+
+
+ 3단계
+
+
+
+
+
+
+
+ */
