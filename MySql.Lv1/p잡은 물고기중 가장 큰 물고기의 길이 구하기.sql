6566663a24b8da6304a9c2168d8f918ee5ab4a94
wnsgud0310 <101302585+wnsgud0310@users.noreply.github.com>
2024-07-18 10:14:30 +0900

    Create p잡은 물고기중 가장 큰 물고기의 길이 구하기.sql
    concat : 문자열 합치기

---
+++ MySql.Lv1/p잡은 물고기중 가장 큰 물고기의 길이 구하기.sql
@@ -0,0 +1,31 @@
+
+select concat(max(LENGTH),'cm') as MAX_LENGTH
+from FISH_INFO;
+
+
+
+
+/*
+ 1단계
+select max(LENGTH) as MAX_LENGTH
+from FISH_INFO;
+
+
+
+ 2단계
+
+길이에 cm 붙이는법 -> concat 키워드 사용
+select concat(max(LENGTH),'cm') as MAX_LENGTH
+from FISH_INFO;
+
+
+
+ 3단계
+
+
+
+
+
+
+
+ */
