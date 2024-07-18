185bed810cd5be74c1582eaff80aa1a4303be8bb
wnsgud0310 <101302585+wnsgud0310@users.noreply.github.com>
2024-07-18 10:26:35 +0900

    Create p한 해에 잡은 물고기 수 구하기.sql

---
+++ MySql.Lv1/p한 해에 잡은 물고기 수 구하기.sql
@@ -0,0 +1,36 @@
+
+select count(id) as FISH_COUNT
+from FISH_INFO
+where TIME Like '%2021%';
+
+
+
+
+
+
+/*
+ 1단계
+select count(id) as FISH_COUNT
+from FISH_INFO
+
+
+
+ 2단계
+select count(id) as FISH_COUNT
+from FISH_INFO
+where TIME Like '%2021%';
+
+
+
+
+
+
+ 3단계
+
+
+
+
+
+
+
+ */
